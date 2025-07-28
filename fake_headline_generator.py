27694813cfbf3f19eef46d66137ec8bb83af7155
lakshya <lakshyabisen1274@gmail.com>
2025-07-28 23:47:16 +0530

    I sucessfully create the List of news

---
+++ fake_headline_generator.py
@@ -0,0 +1,36 @@
+import random
+
+
+
+subjects = [
+    "Elon Musk", 
+    "Virat Kohli",
+    "NASA",
+    "A Mumabai Cat",
+    "A Group of Monkeys",
+    "Prime Minsiter modi",
+    "Auto Rickshaw Driver from Delhi"
+]
+
+
+actions = [
+    "launches",
+    "cancels",
+    "dances with",
+    "eats",
+    "declares war on",
+    "orders",
+    "celebrates"
+    
+]
+
+places_or_things = [
+    "Red Fort",
+    "In Mumbai local Train",
+    "a plote of samosa",
+    "inside parliament",
+    "at ganga ghat",
+    "during IPL Match",
+    "at India Gate"
+]
+
